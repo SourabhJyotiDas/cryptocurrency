7144a1d4f25fd9e2c06e9acff2b6ddaf6794955b
Sourabh Jyoti Das <dassourabh126@gmail.com>
2023-02-01 10:29:40 +0530

    4th commit

--- src/components/Blogs.js
+++ src/components/Blogs.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Blogs() {
     return (
         <>
-        hello
+        hello 4th
             <div>
                 <div className=' flex flex-col justify-center items-start   m-10 lg:px-10'>
                     <h3 className='tracking-widest font-bold text-xs md:text-sm lg:text-base md:leading-loose'>&#9830; Introduction to Cryptocurrency and Blockchain Technology</h3>
